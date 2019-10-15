4c460759b1cf891648880703d350bcd89d69bc3e
ramideltoro <rami.deltoro@gmail.com>
2019-10-14 20:25:20 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from pandas import read_csv
+from matplotlib import pyplot
+import scipy
+
+file_name = "/Users/ramideltoro/ml/diabetes.csv"
+raw_data = open(file_name, 'rt')
+names = ['preg','plas','pres','skin','test','mass','pedi','age','class']
+data = read_csv(file_name,names=names)
+
+data.plot(kind='density', subplots=True, layout=(3,3), sharex=False)
+
+pyplot.show() 
