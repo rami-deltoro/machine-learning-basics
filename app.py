ef82c1fc3b8acf8525720784076a324c8236bec0
ramideltoro <rami.deltoro@gmail.com>
2019-10-15 20:31:58 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from pandas import read_csv
+from numpy import set_printoptions
+from sklearn.preprocessing import MinMaxScaler
+
+file_name = "/Users/ramideltoro/ml/diabetes.csv"
+raw_data = open(file_name, 'rt')
+names = ['preg','plas','pres','skin','test','mass','pedi','age','class']
+data = read_csv(file_name,names=names)
+
+data_array = data.values
+
+
+#split array to input & output
+
+input_array = data_array[:,0:8]
+output_array = data_array[:,8]
+
+scaler = MinMaxScaler(feature_range=(0,1))
+
+rescaled_input_array = scaler.fit_transform(input_array)
+
+set_printoptions(precision=3)
+
+print(rescaled_input_array)
